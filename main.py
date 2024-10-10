13c5edc3910a9720032d70ffd78d358303c61c9d
Case Howard-Moreno <cahoward-moreno@stu.lltc.edu>
2024-10-10 10:17:48 -0500

    final commit

---
+++ main.py
@@ -0,0 +1,31 @@
+def main():
+    a = 20.00
+    b = 20
+    c = 10
+    d = 10.00
+
+    print(a/c) #2.00
+    print(a/d) #2.00
+    print(b/c) #2
+    print(b/d) #2.00
+    
+    print(a*c) #200.00
+    print(a*d) #200.00
+    print(b*c) #200
+    print(b*d) #200.00
+
+    print(a+c) #30.00
+    print(a+d) #30.00
+    print(b+c) #30
+    print(b+d) #30.00
+
+    print(a-c) #10.00
+    print(a-d) #10.00
+    print(b-c) #10
+    print(b-d) #10.00
+
+
+    #repeat this for *, +, and -
+
+if __name__ == "__main__":
+    main()
